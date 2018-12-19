b64523ca9db7c3f77852bb0f124405d2c00eaecd
Aleasdeck <aleasdeck@gmail.com>
2018-12-20 01:30:06 +1000

    !

--- src/paket/Commands.java
+++ src/paket/Commands.java
@@ -1,9 +1,6 @@
 package paket;
 
-import net.minecraft.server.v1_12_R1.EntityCaveSpider;
-import net.minecraft.server.v1_12_R1.EntityCreature;
-import net.minecraft.server.v1_12_R1.PathfinderGoalMeleeAttack;
-import net.minecraft.server.v1_12_R1.WorldServer;
+import net.minecraft.server.v1_12_R1.*;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.command.Command;
@@ -35,22 +32,33 @@ public class Commands implements CommandExecutor{
         Location loc = player.getLocation();
         WorldServer world = ((CraftWorld)player.getWorld()).getHandle();
 
+
+
         cc = new EntityCaveSpider(world);
         c = (EntityCreature) cc;
 
-        c.setNoAI(true);
+        CaveSpider csMike = (CaveSpider) cc.getBukkitEntity();
 
-        //c.setLocation(loc.getX(), loc.getY(), loc.getZ(), 0,0);
+        csMike.setMaxHealth(100);
+        c.setHealth(100);
+
+        c.setCustomName(ChatColor.RED + "Mike");
+        c.setCustomNameVisible(true);
+
+        //c.goalSelector.a(4, new PathfinderGoalTame((EntityCreature)c, 1.0D, true));
+
+        c.goalSelector.a(4, new PathfinderGoalMeleeAttack((EntityCreature)c, 1.0D, true));
+        c.targetSelector.a(2, new PathfinderGoalNearestAttackableTarget<EntityPig>((EntityCreature) c, EntityPig.class, true));
+
+        c.setLocation(loc.getX(), loc.getY(), loc.getZ(), 0,0);
         c.getNavigation().a(loc.getX(), loc.getY(), loc.getZ());
         c.setLocation(loc.getX(), loc.getY(), loc.getZ(), loc.getYaw(), loc.getPitch());
 
         //CaveSpider csMike = (CaveSpider) cc.getBukkitEntity();
         //csMike.setMaxHealth(100);
-        c.setHealth(100);
-        c.setCustomName(ChatColor.RED + "Mike");
-        c.setCustomNameVisible(true);
 
         world.addEntity(c);
+        csMike.setPassenger(player);
 
         return true;
     }
--- src/paket/Handlers.java
+++ src/paket/Handlers.java
@@ -4,13 +4,18 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
 
 public class Handlers implements Listener {
 
     @EventHandler
     public void onHitByPetOwner(EntityDamageByEntityEvent event) {
-        if(!(event.getDamager() instanceof Player)) return;
+        if (!(event.getDamager() instanceof Player)) return;
         Player owner = (Player) event.getDamager();
+    }
 
-            }
+    @EventHandler
+    public void onOwnerMove(PlayerMoveEvent e) {
+
+    }
 }
